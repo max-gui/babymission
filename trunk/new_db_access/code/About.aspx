1d8e6161ec41381bb0a2526a6406d3be6aeb167f
playstation.gui <playstation.gui@14049fcb-0f85-fc1e-373c-64ba4d7b927f>
2010-06-11 09:18:51 +0000

    try to let the db add、del、update accept at once;

---
+++ trunk/new_db_access/code/About.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="关于我们" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeFile="About.aspx.cs" Inherits="About" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        关于
+    </h2>
+    <p>
+        寡人多年以后的又一个 二货网站！</p>
+</asp:Content>
---
+++ trunk/new_db_access/code/App_Code/logic/AuthProcess.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Configuration;
+using System.Data;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+/// <summary>
+///AuthProcess 的摘要说明
+/// </summary>
+public class AuthProcess : SelectLogic
+{
+	public AuthProcess(DataSet dataSet)
+        : base(dataSet)
+	{
+		//
+		//TODO: 在此处添加构造函数逻辑
+		//
+        ta = (tbl_authority)InitDatabaseProc("tbl_authority");
+	}
+
+    private tbl_authority ta = null;
+
+    public override void Process()
+    {
+    }
+
+    //public void commit()
+    //{
+    //    tua.SelectUsrAuthCommit(MyDst);
+    //}
+
+    public override void Add()
+    {
+        //TAB_DATA_USERDatabase db = (TAB_DATA_USERDatabase)InitDatabaseProc("Database", "DataBase.TAB_DATA_USERDatabase");
+        //TAB_DATA_LOG dbLog = (TAB_DATA_LOG)InitDatabaseProc("Database", "DataBase.TAB_DATA_LOG");
+
+        //db.Insert(MyDst, int.Parse(StrRtn), dbLog);
+    }
+
+    public override void Del()
+    {
+        //TAB_DATA_USERDatabase db = (TAB_DATA_USERDatabase)InitDatabaseProc("Database", "DataBase.TAB_DATA_USERDatabase");
+        //TAB_DATA_LOG dbLog = (TAB_DATA_LOG)InitDatabaseProc("Database", "DataBase.TAB_DATA_LOG");
+        //db.Delete(int.Parse(StrRtn), IntRtn, dbLog);
+    }
+
+    public override void Updata()
+    {
+        //TAB_DATA_USERDatabase db = (TAB_DATA_USERDatabase)InitDatabaseProc("Database", "DataBase.TAB_DATA_USERDatabase");
+        //TAB_DATA_LOG dbLog = (TAB_DATA_LOG)InitDatabaseProc("Database", "DataBase.TAB_DATA_LOG");
+
+        //db.UpdataUser(this.MyDst, int.Parse(StrRtn), dbLog);
+    }
+
+    public override void PwdEdit()
+    {
+        //TAB_DATA_USERDatabase db = (TAB_DATA_USERDatabase)InitDatabaseProc("Database", "DataBase.TAB_DATA_USERDatabase");
+        //db.UpdataPwd(this.MyDst);
+    }
+
+    public override void View()
+    {
+        MyDst = ta.SelectAuthorityView();
+    }
+
+    public override void Search()
+    {
+        //TAB_DATA_USERDatabase db = (TAB_DATA_USERDatabase)InitDatabaseProc("Database", "DataBase.TAB_DATA_USERDatabase");
+        //this.MyDst = db.SelectComb(int.Parse(StrRtn));
+    }
+}
